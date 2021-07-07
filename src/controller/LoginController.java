476602c6bea1741acbe7e3de49dde7ce1be016ac
Feri80 <bigfarhad7780@gmail.com>
2021-07-07 21:55:29 +0430

    login view & controller added

---
+++ src/controller/LoginController.java
@@ -0,0 +1,76 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class LoginController 
+{
+
+    @FXML
+    private TextField loginUsernameTextField;
+
+    @FXML
+    private PasswordField loginPasswordTextField;
+
+    @FXML
+    private ImageView loginButton;
+
+    @FXML
+    private TextField signUpUsernameTextField;
+
+    @FXML
+    private PasswordField signUpPasswordTextField;
+
+    @FXML
+    private ImageView signUpButton;
+
+    @FXML
+    private PasswordField signUpConfirmTextField;
+
+    @FXML
+    void loginButtonClicked(MouseEvent event) 
+    {
+
+    }
+
+    @FXML
+    void loginButtonPressed(MouseEvent event) 
+    {
+
+    }
+
+    @FXML
+    void loginButtonReleased(MouseEvent event) 
+    {
+
+    }
+
+    @FXML
+    void signUpButtonClicked(MouseEvent event) 
+    {
+
+    }
+
+    @FXML
+    void signUpButtonPressed(MouseEvent event) 
+    {
+
+    }
+
+    @FXML
+    void signUpButtonReleased(MouseEvent event) 
+    {
+
+    }
+
+    @FXML
+    void initialize()
+    {
+        
+    }
+
+}
+
