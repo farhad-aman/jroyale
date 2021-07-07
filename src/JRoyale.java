cd912593362cefafc38917352dd6a7b9a9005c24
Feri80 <bigfarhad7780@gmail.com>
2021-07-07 15:33:07 +0430

    first commit

---
+++ src/JRoyale.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JRoyale extends Application
+{
+    @Override
+    public void start(Stage primaryStage) throws Exception 
+    {
+        
+    }
+
+    public static void main(String[] args) 
+    {
+        launch(args);    
+    }
+}
