156507018a88d423ac81432e700caa8858deaeed
Feri80 <bigfarhad7780@gmail.com>
2021-07-12 15:44:40 +0430

    menu fxml skeleton completed

---
+++ src/JRoyale/MenuController.java
@@ -0,0 +1,235 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class MenuController 
+{
+
+    @FXML
+    private PasswordField oldPasswordField;
+
+    @FXML
+    private PasswordField newPasswordField;
+
+    @FXML
+    private PasswordField confirmPasswordField;
+
+    @FXML
+    private ImageView changePasswordButton;
+
+    @FXML
+    private Label usernameValue;
+
+    @FXML
+    private Label xpValue;
+
+    @FXML
+    private ImageView levelImageView;
+
+    @FXML
+    private ImageView archerImageView;
+
+    @FXML
+    private ImageView barbariansImageView;
+
+    @FXML
+    private ImageView wizardImageView;
+
+    @FXML
+    private ImageView dragonImageView;
+
+    @FXML
+    private ImageView giantImageView;
+
+    @FXML
+    private ImageView pekkaImageView;
+
+    @FXML
+    private ImageView cannonImageView;
+
+    @FXML
+    private ImageView valkyrieImageView;
+
+    @FXML
+    private ImageView fireballImageView;
+
+    @FXML
+    private ImageView arrowsImageView;
+
+    @FXML
+    private ImageView infernoImageView;
+
+    @FXML
+    private ImageView rageImageView;
+
+    @FXML
+    private ImageView card1;
+
+    @FXML
+    private ImageView card5;
+
+    @FXML
+    private ImageView card2;
+
+    @FXML
+    private ImageView card6;
+
+    @FXML
+    private ImageView card3;
+
+    @FXML
+    private ImageView card7;
+
+    @FXML
+    private ImageView card4;
+
+    @FXML
+    private ImageView card8;
+
+    @FXML
+    private CheckBox archerCheckBox;
+
+    @FXML
+    private CheckBox barbariansCheckBox;
+
+    @FXML
+    private CheckBox dragonCheckBox;
+
+    @FXML
+    private CheckBox wizardCheckBox;
+
+    @FXML
+    private CheckBox cannonCheckBox;
+
+    @FXML
+    private CheckBox valkyrieCheckBox;
+
+    @FXML
+    private CheckBox pekkaCheckBox;
+
+    @FXML
+    private CheckBox giantCheckBox;
+
+    @FXML
+    private CheckBox fireballCheckBox;
+
+    @FXML
+    private CheckBox arrowsCheckBox;
+
+    @FXML
+    private CheckBox rageCheckBox;
+
+    @FXML
+    private CheckBox infernoCheckBox;
+
+    @FXML
+    private ImageView saveDeckButton;
+
+    @FXML
+    private TableView<?> historyTable;
+
+    @FXML
+    private TableColumn<?, ?> botDifficultyColumn;
+
+    @FXML
+    private TableColumn<?, ?> playerStarsColumn;
+
+    @FXML
+    private TableColumn<?, ?> botStarsColumn;
+
+    @FXML
+    private TableColumn<?, ?> winnerColumn;
+
+    @FXML
+    private ImageView easyButton;
+
+    @FXML
+    private ImageView normalButton;
+
+    @FXML
+    private ImageView hardButton;
+
+    @FXML
+    void changePasswordButtonPressed(MouseEvent event) 
+    {
+
+    }
+
+    @FXML
+    void changePasswordButtonReleased(MouseEvent event) 
+    {
+
+    }
+
+    @FXML
+    void confirmPasswordFieldPressed(MouseEvent event) 
+    {
+
+    }
+
+    @FXML
+    void easyButtonPressed(MouseEvent event) 
+    {
+
+    }
+
+    @FXML
+    void easyButtonReleased(MouseEvent event) 
+    {
+
+    }
+
+    @FXML
+    void hardButtonPressed(MouseEvent event) 
+    {
+
+    }
+
+    @FXML
+    void hardButtonReleased(MouseEvent event) 
+    {
+
+    }
+
+    @FXML
+    void newPasswordFieldPressed(MouseEvent event) 
+    {
+
+    }
+
+    @FXML
+    void normalButtonPressed(MouseEvent event) 
+    {
+
+    }
+
+    @FXML
+    void normalButtonReleased(MouseEvent event) 
+    {
+
+    }
+
+    @FXML
+    void oldPasswordFieldPressed(MouseEvent event) 
+    {
+
+    }
+
+    @FXML
+    void saveDeckButtonPressed(MouseEvent event) 
+    {
+
+    }
+
+    @FXML
+    void saveDeckButtonReleased(MouseEvent event) 
+    {
+
+    }
+
+}
